804c20c650d2b2325dc6ed505d1e84ec450644a4
sayak23082002 <sayakkarui981@gmail.com>
2024-09-26 23:24:50 +0530

    Bable Update

--- webpack.config.js
+++ webpack.config.js
@@ -41,9 +41,20 @@ const config = {
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
     ],
   },
   resolve: {
+    extensions: ['.js', '.jsx'],
   fallback: {
     path: require.resolve('path-browserify'),
     os: require.resolve('os-browserify/browser'),
